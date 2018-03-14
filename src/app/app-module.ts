1dabb1c5ebe7f07608fde568fc350513fc4d849d
soloproyectos <gonzalo.chumillas@gmail.com>
2018-03-14 22:51:15 +0100

    i18n

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 
 import { AppRoutingModule } from './app-routing-module';
@@ -7,6 +10,10 @@ import { AppComponent } from './app-component';
 import { ViewComponent as HomeComponent } from './views/home/view';
 import { ViewComponent as PageNotFoundComponent } from './views/not-found/view';
 
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -16,8 +23,16 @@ import { ViewComponent as PageNotFoundComponent } from './views/not-found/view';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     ScrollToModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
